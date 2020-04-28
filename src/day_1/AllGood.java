901772b178a6cfff98180628f3de70c63b61d749
spingul <62077760+spingul@users.noreply.github.com>
2020-04-28 16:24:37 -0400

    Update AllGood.java

--- src/day_1/AllGood.java
+++ src/day_1/AllGood.java
@@ -5,6 +5,17 @@ public class AllGood {
     public static void main(String[] args) {
 
         // I hope everything is Good.
+        
+        System.out.println("Hello dear folks");
+                System.out.println("Hello dear folks");
+        System.out.println("Hello dear folks");
+        System.out.println("Hello dear folks");
+        System.out.println("Hello dear folks");
+        System.out.println("Hello dear folks");
+        System.out.println("Hello dear folks");
+        System.out.println("Hello dear folks");
+        System.out.println("Hello dear folks");
+
 
     }
 }
